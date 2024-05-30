0a3391f67c27392eca0e1fe1ae6f97f0b3b674d1
Prindl <maximilianprindl@hotmail.com>
2024-05-30 11:19:47 +0200

    Fixed name change. Added comment for star representation.

--- src/star.py
+++ src/star.py
@@ -7,9 +7,24 @@ From the paper:
 
 The Star representation:
 ========================
+The representation is a modification of the object points such that rotating by one step of symmetry, i.e. 2π/n , is a
+simple closed curve in the representation. In it, all object points, that appear the same (based on the
+defined symmetry), are mapped on the same value and no possible rotation will result in an uncontinuous change.
+Therefore, the representation becomes symmetry aware, but also ambiguous. To gain the star representation of the object
+points, these are first transformed in cylindrical coordinate space, where the cylindric axis is aligned with the symmetry axis.
+Here the angle value is multiplied by n (the fold of symmetry). Afterwards the points are transformed back to Cartesian vector space.
 
+                         [n    ]
+    po_ij* = cartetsian( [  1  ] cylindric(po_ij) )      Note: This assumes Z as symmetry axis.
+                         [    1]
+
+Let’s have a closer look at the folds of symmetry extremes:
+On the lower end, one finds non-symmetrical objects (n = 1); In this case the star representation is identical to the
+origin object points which is the expected outcome. On the other end, we find objects with infinity-fold symmetries, e.g.
+bottles. Here an infinitely small step of rotation closes one step of symmetry. Since the multiplication with infinity is
+unhandy, in this case, we multiply the angle values with zero. Therefore, all points have the same angle around the
+rotation axis as they all are equivalent under symmetry.
 """
-import json
 import numpy as np
 import math
 
@@ -17,9 +32,9 @@ import utils
 
 def collapses_obj_to_dot_symmetry(obj, x_factor=1, y_factor=1, z_factor=1):
     R = utils.eye(3, batch_shape=obj.shape[:-1])
-    obj = utils.change_Angle_around_Axis(R[...,0], obj, R[...,1], x_factor)
-    obj = utils.change_Angle_around_Axis(R[...,1], obj, R[...,2], y_factor)
-    obj = utils.change_Angle_around_Axis(R[...,2], obj, R[...,0], z_factor)
+    obj = utils.change_angle_around_axis(R[...,0], obj, R[...,1], x_factor)
+    obj = utils.change_angle_around_axis(R[...,1], obj, R[...,2], y_factor)
+    obj = utils.change_angle_around_axis(R[...,2], obj, R[...,0], z_factor)
     return obj
 
 def calculate(model_info, po_image):
